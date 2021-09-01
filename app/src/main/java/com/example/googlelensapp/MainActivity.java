a39937c4fe52e466c747b4f8b0bca51eccedac78
AbhaySingh5349 <abhay5349singh@gmail.com>
2021-09-02 01:42:41 +0530

    Searched item layout

--- app/src/main/java/com/example/googlelensapp/MainActivity.java
+++ app/src/main/java/com/example/googlelensapp/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.googlelensapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.googleLens)
+    ImageView googleLens;
+    @BindView(R.id.snapBtn)
+    Button snapBtn;
+    @BindView(R.id.resultsBtn)
+    Button resultsBtn;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
